c0c59f1979bef1b09450fa78b1ac327238d41616
Risda Tamam <risda123tamam@gmail.com>
2021-05-14 16:03:22 +0700

    Create HomePage php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
